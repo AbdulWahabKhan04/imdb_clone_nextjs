2ebe957df9421e731a291bfec4b14dcdc50982cc
Abdul Wahab Khan <asad49033@gmail.com>
2024-12-29 18:36:59 +0500

    created the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='flex flex-col max-w-6xl mx-auto p-5'>
+        <h1 className="text-2xl font-medium text-amber-600">About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit harum iste cumque assumenda, laudantium veritatis quas quibusdam dolore! Dolore saepe tenetur animi in. Aperiam ea possimus distinctio fuga facilis consequatur.
+        Ratione amet doloribus quae recusandae at quos quisquam, minima sequi ea sunt quam ipsum culpa nihil voluptatibus ut ipsa accusamus aperiam vel voluptates delectus, eius, natus neque. Iure, neque numquam!</p>
+    </div>
+  )
+}
